72867d5480d25057ea504b287f2a86a1c3f1867c
tinmanSimon <wudisas@gmail.com>
2023-02-28 20:56:45 -0800

    able to load images and save images and some stuffs

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
 
 import os
-imagePath = r'C:\Users\wudis\OneDrive\currentPC\Pictures\tmp'
+from PIL import Image
+imagePath = 'C:\\Users\\wudis\\OneDrive\\currentPC\\Pictures\\tmp\\'
+saveImagePath = 'C:/Users/wudis/OneDrive/currentPC/Documents/python/photoAssemble/saved'
 
 def getImages(path):
     res = []
     for file in os.listdir(path):
          if os.path.isfile(os.path.join(path, file)):
-            res.append(file)
+            res.append(path + file)
+    return [Image.open(file) for file in res]
+
+def concatRight(im1, im2):
+    dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
+
+def concatDown(im1, im2):
+    dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
+
+# expecting a Pillow Image object
+def getCroppedImages(image, croppedLen):
+    w, h = image.size
+    res = []
+    curh, curw = 0, 0
+    while curh < h:
+        while curw < w:
+            # todo crop
+            croppedImg = image.crop(curw, curh, curw + croppedLen, curh + croppedLen)
+            if h < croppedLen or curW < croppedLen:
+                croppedImg.resize((croppedLen, croppedLen))
+            res.append(croppedImg)
+            curw += croppedLen
+        curh += croppedLen
     return res
 
+
+def saveImgs(dir, images):
+    #os.mkdir(dir)
+    for i, image in enumerate(images):
+        image.save(dir + "/" + str(i) + ".png")
+
 images = getImages(imagePath)
-#print(images)
+saveImgs(saveImagePath, images)
+print(len(images))
 
